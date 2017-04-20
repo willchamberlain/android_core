6c131097513b5bc9dc528433dce08147087ee0e1
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2017-04-20 15:02:50 +1000

    rename to make window size clear

--- androidvosopencvros/src/main/java/william/chamberlain/androidvosopencvros/DataTrack.java
+++ androidvosopencvros/src/main/java/william/chamberlain/androidvosopencvros/DataTrack.java
@@ -5,7 +5,7 @@ package william.chamberlain.androidvosopencvros;
  *
  * constructor(data streams) : hooks into other data streams
  *
- * fixed-size array(s) - one per data element : initially empty
+ * fixed-windowSize array(s) - one per data element : initially empty
  *
  * synchronized add(data): check for full array; if full, copy all-but-first to new array, add new data to end of array
  *
@@ -14,24 +14,24 @@ class DataTrack {
 
 
     private DetectedFeature[] data;
-    private int size;
+    private int windowSize;
     private int sizeNow;
 
     public DataTrack(int size_) {
-        size = size_;
+        windowSize = size_;
         data = new DetectedFeature[size_];
         sizeNow=0;
     }
 
     public synchronized void add(DetectedFeature detectedFeature){  //  TODO: not efficient - look at circular lists / circular queues
-        if(sizeNow < size) {
+        if(sizeNow < windowSize) {
             sizeNow++;
             data[sizeNow-1]=detectedFeature;
         } else {
             DetectedFeature[] newData = new DetectedFeature[sizeNow];
-            for (int i_ = 1; i_ < sizeNow; i_++) {       //  e.g. if size = 10    1 ... 9
-                newData[i_ - 1] = data[i_];              //  e.g. if size = 10    newData[0]=data[1] ... newData[8]=data[9]
-                newData[sizeNow-1] = detectedFeature;    //  e.g. if size = 10    newData[9]=detectedFeature
+            for (int i_ = 1; i_ < sizeNow; i_++) {       //  e.g. if windowSize = 10    1 ... 9
+                newData[i_ - 1] = data[i_];              //  e.g. if windowSize = 10    newData[0]=data[1] ... newData[8]=data[9]
+                newData[sizeNow-1] = detectedFeature;    //  e.g. if windowSize = 10    newData[9]=detectedFeature
                 data = newData;
             }
         }
@@ -42,7 +42,7 @@ class DataTrack {
      * @return
      */
     public DetectedFeature[] data() {
-        if(sizeNow < size) {
+        if(sizeNow < windowSize) {
             DetectedFeature[] newData = new DetectedFeature[sizeNow];
             for (int i_ = 0; i_ < sizeNow; i_++) {    //  e.g. if sizeNow = 7    0 ... 6
                 newData[i_] = data[i_];              //  e.g. if sizeNow = 7    newData[0]=data[0] ... newData[6]=data[6]
