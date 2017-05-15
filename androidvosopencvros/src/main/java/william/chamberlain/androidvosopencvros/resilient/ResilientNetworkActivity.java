d56c6b9404380e39990f9b477282105668c4b22c
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2017-05-15 11:43:05 +1000

    Resilient networking: part 1 - incomplete - ResilientNetworkActivity for networking options on Activities.

---
+++ androidvosopencvros/src/main/java/william/chamberlain/androidvosopencvros/resilient/ResilientNetworkActivity.java
@@ -0,0 +1,11 @@
+package william.chamberlain.androidvosopencvros.resilient;
+
+import java.net.URI;
+
+/**
+ * Created by will on 15/05/17.
+ */
+
+public interface ResilientNetworkActivity {
+    URI[] possibleMasterUris();
+}
