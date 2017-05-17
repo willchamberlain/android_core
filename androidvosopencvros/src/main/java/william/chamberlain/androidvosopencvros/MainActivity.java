7655ed6299c486d50422d177d8141dd06a3defbf
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2017-05-17 18:47:32 +1000

    Y rotation is still negated, and axes are through the back of the tag, but good enough

--- androidvosopencvros/src/main/java/william/chamberlain/androidvosopencvros/MainActivity.java
+++ androidvosopencvros/src/main/java/william/chamberlain/androidvosopencvros/MainActivity.java
@@ -62,6 +62,8 @@ import boofcv.struct.image.ImageGray;
 import boofcv.struct.image.ImageType;
 import geometry_msgs.Pose;
 import georegression.geometry.ConvertRotation3D_F64;
+import georegression.geometry.GeometryMath_F64;
+import georegression.struct.EulerType;
 import georegression.struct.point.Point2D_F64;
 import georegression.struct.point.Vector3D_F64;
 import georegression.struct.se.Se3_F64;
@@ -97,7 +99,7 @@ public class MainActivity
         DimmableScreen, VariableResolution, VisionSource,
         ResilientNetworkActivity {
 
-    public static final int CAMERA_FRONT_OR_BACK_INIT = CAMERA_ID_FRONT;
+    public static final int CAMERA_FRONT_OR_BACK_INIT = CAMERA_ID_BACK;
     private static final boolean running_native = false;
 
 
@@ -213,7 +215,7 @@ public class MainActivity
 
         _cameraBridgeViewBase = (CameraBridgeViewBase) findViewById(R.id.main_surface);
         _cameraBridgeViewBase.setCameraIndex(CAMERA_FRONT_OR_BACK_INIT);  // front-facing camera is the user-side camera i.e. facing toward the front of the device from the user point of view and away from the front of the device from the device point of view, back-facing/rear-facing is usually the primary camera facing away from the user i.e. facing away from the rear of the device
-        System.out.println("MainActivity: onCreate: after running _cameraBridgeViewBase.setCameraIndex(CAMERA_ID_BACK)");
+        System.out.println("MainActivity: onCreate: after running _cameraBridgeViewBase.setCameraIndex("+CAMERA_FRONT_OR_BACK_INIT+")");
         System.out.println("MainActivity: onCreate: before running _cameraBridgeViewBase.setMaxFrameSize("+ MAX_FRAME_SIZE_WIDTH_INIT +","+ MAX_FRAME_SIZE_HEIGHT_INIT +")");
         _cameraBridgeViewBase.setMaxFrameSize(MAX_FRAME_SIZE_WIDTH_INIT, MAX_FRAME_SIZE_HEIGHT_INIT);  // http://stackoverflow.com/questions/17868954/android-opencv-how-to-set-camera-resolution-when-using-camerabridgeviewbase
         System.out.println("MainActivity: onCreate: after running _cameraBridgeViewBase.setMaxFrameSize("+ MAX_FRAME_SIZE_WIDTH_INIT +","+ MAX_FRAME_SIZE_HEIGHT_INIT +")");
@@ -573,10 +575,17 @@ public class MainActivity
                 float  px_pixels = (float)(matGray.size().width/2.0);
                 float  py_pixels = (float)(matGray.size().height/2.0);
                 double skew = 0.0;
-                CameraPinhole pinholeModel = new CameraPinhole(focal_length_in_pixels_x,focal_length_in_pixels_y,skew,px_pixels,py_pixels,new Double(matGray.size().width).intValue(),new Double(matGray.size().height).intValue());
+
+
+                CameraPinhole pinholeModel = new CameraPinhole(
+                        focal_length_in_pixels_x, focal_length_in_pixels_y,
+                        skew,
+                        px_pixels,py_pixels,
+                        new Double(matGray.size().width).intValue(),new Double(matGray.size().height).intValue());
                 LensDistortionNarrowFOV pinholeDistort = new LensDistortionPinhole(pinholeModel);
                 detector.setLensDistortion(pinholeDistort);  // TODO - do BoofCV calibration - but assume perfect pinhole camera for now
                 detector.detect(image);
+
                 Log.i(TAG, "last_frame_bytes: found "+detector.totalFound()+" tags via BoofCV");
 
                 // see https://boofcv.org/index.php?title=Example_Fiducial_Square_Image
@@ -605,28 +614,100 @@ public class MainActivity
 
                         Vector3D_F64 transBoofCV_TtoS = targetToSensor.getTranslation();
                         Quaternion_F64 quatBoofCV_TtoS = new Quaternion_F64();
-                        ConvertRotation3D_F64.matrixToQuaternion(targetToSensor.getR(),quatBoofCV_TtoS);
-                        System.out.println("3D Location: targetToSensor : BoofCV frame : x = "+transBoofCV_TtoS.getX()+", y = "+transBoofCV_TtoS.getY()+", z = "+transBoofCV_TtoS.getZ());
-                        System.out.println("3D Location: targetToSensor : BoofCV frame : qx = "+quatBoofCV_TtoS.x+", qy = "+quatBoofCV_TtoS.y+", qz = "+quatBoofCV_TtoS.z+", qw = "+quatBoofCV_TtoS.w);
+                        ConvertRotation3D_F64.matrixToQuaternion(targetToSensor.getR(), quatBoofCV_TtoS);
+                        System.out.println("3D Location: targetToSensor : BoofCV frame : x = " + transBoofCV_TtoS.getX() + ", y = " + transBoofCV_TtoS.getY() + ", z = " + transBoofCV_TtoS.getZ());
+                        System.out.println("3D Location: targetToSensor : BoofCV frame : qx = " + quatBoofCV_TtoS.x + ", qy = " + quatBoofCV_TtoS.y + ", qz = " + quatBoofCV_TtoS.z + ", qw = " + quatBoofCV_TtoS.w);
 
-                        Se3_F64 sensorToTargetIn = null;
+                        // 7210 is the translation and pose straight from BoofCV in tag-to-sensor
+                        detectedFeaturesClient.reportDetectedFeature(7000+tag_id,
+                                transBoofCV_TtoS.getX(), transBoofCV_TtoS.getY(), transBoofCV_TtoS.getZ(),
+                                quatBoofCV_TtoS.x,quatBoofCV_TtoS.y,quatBoofCV_TtoS.z,quatBoofCV_TtoS.w);
+
+
+                        Se3_F64 sensorToTargetIn;
+                        sensorToTargetIn = null;
                         Se3_F64 sensorToTarget;
+                        sensorToTarget = targetToSensor.invert(sensorToTargetIn);
+                        Vector3D_F64 transBoofCV_StoT = sensorToTarget.getTranslation();
+                        Quaternion_F64 quatBoofCV_StoT = new Quaternion_F64();
+                        ConvertRotation3D_F64.matrixToQuaternion(targetToSensor.getR(), quatBoofCV_TtoS);
+                        detectedFeaturesClient.reportDetectedFeature(8000+tag_id,
+                                transBoofCV_StoT.getX(), transBoofCV_StoT.getY(), transBoofCV_StoT.getZ(),
+                                quatBoofCV_StoT.x,quatBoofCV_StoT.y,quatBoofCV_StoT.z,quatBoofCV_StoT.w);
 
-                        DenseMatrix64F rotNeg90Y = new DenseMatrix64F( new double[][] { {0,0,-1} , {0,1,0} , {1,0,0} });
-                        DenseMatrix64F rotNeg90Z = new DenseMatrix64F( new double[][] { {0,1,0} , {-1,0,0} , {0,0,1} });
-                        DenseMatrix64F temp = CommonOps.identity(3) ;
-                        DenseMatrix64F new_targetToSensor_r = CommonOps.identity(3) ;
-                        CommonOps.mult(rotNeg90Z,rotNeg90Y,temp);
-                        CommonOps.mult(targetToSensor.getR(),temp,new_targetToSensor_r);
+                        DenseMatrix64F transformation_fromBoofCVFiducialTagToSensor_toRobotSensorToTag
+                                = new DenseMatrix64F(new double[][]{
+                                {  0.0 ,  0.0 , -1.0 } ,
+                                { -1.0 ,  0.0 ,  0.0 } ,
+                                {  0.0 , +1.0 ,  0.0} });
 
-                        targetToSensor.setRotation(new_targetToSensor_r);
 
+                        Se3_F64 targetToSensorViaTransform = new Se3_F64();
+                        DenseMatrix64F targetToSensorViaTransformRot = CommonOps.identity(3);
+                        CommonOps.mult(transformation_fromBoofCVFiducialTagToSensor_toRobotSensorToTag,targetToSensor.getR(),targetToSensorViaTransformRot);
+
+                        Se3_F64 sensorToTargetViaTransform = new Se3_F64();
+                        DenseMatrix64F sensorToTargetViaTransformRot = CommonOps.identity(3);
+                        CommonOps.mult(transformation_fromBoofCVFiducialTagToSensor_toRobotSensorToTag,targetToSensor.getR(),sensorToTargetViaTransformRot);                        DenseMatrix64F sensorToTargetViaTransformRotInverted = CommonOps.identity(3);
+
+                        CommonOps.invert(sensorToTargetViaTransformRot);
+
+                        sensorToTargetViaTransform.setRotation(sensorToTargetViaTransformRot);
+                        Vector3D_F64 sensorToTargetViaTransformTrans = new Vector3D_F64();
+                        sensorToTargetViaTransform.setTranslation(transBoofCV_TtoS.getZ(), -1.0*transBoofCV_TtoS.getX(), -1.0*transBoofCV_TtoS.getY());
+                        Quaternion_F64 sensorToTargetViaTransformQuat = new Quaternion_F64();
+                        ConvertRotation3D_F64.matrixToQuaternion(sensorToTargetViaTransformRot, sensorToTargetViaTransformQuat);
+
+                        detectedFeaturesClient.reportDetectedFeature(9000+tag_id,
+                                sensorToTargetViaTransform.getX(), sensorToTargetViaTransform.getY(), sensorToTargetViaTransform.getZ(),
+                                sensorToTargetViaTransformQuat.x,sensorToTargetViaTransformQuat.y,sensorToTargetViaTransformQuat.z,sensorToTargetViaTransformQuat.w);
+
+                        double[] eulerBefore=new double[]{0,0,0};
+                        ConvertRotation3D_F64.matrixToEuler(sensorToTargetViaTransformRot,EulerType.XYZ,eulerBefore);
+//                        double[] eulerAfter = new double[] { -1.0*eulerBefore[2], -1.0*eulerBefore[0], eulerBefore[1]};
+                        double[] eulerAfter = new double[] { -1.0*eulerBefore[2], 0.0, 0.0};
+                        ConvertRotation3D_F64.eulerToMatrix(EulerType.XYZ,eulerAfter[0],eulerAfter[1],eulerAfter[2],sensorToTargetViaTransformRot);
+                        sensorToTargetViaTransform.setRotation(sensorToTargetViaTransformRot);
+                        ConvertRotation3D_F64.matrixToQuaternion(sensorToTargetViaTransformRot, sensorToTargetViaTransformQuat);
+
+                        detectedFeaturesClient.reportDetectedFeature(10000+tag_id,
+                                sensorToTargetViaTransform.getX(), sensorToTargetViaTransform.getY(), sensorToTargetViaTransform.getZ(),
+                                sensorToTargetViaTransformQuat.x,sensorToTargetViaTransformQuat.y,sensorToTargetViaTransformQuat.z,sensorToTargetViaTransformQuat.w);
+
+
+
+                            // transform the rotation to robot coordinate frame convention
+                            DenseMatrix64F rotNeg90Y = new DenseMatrix64F(new double[][]{{0, 0, -1}, {0, 1, 0}, {1, 0, 0}});
+                            DenseMatrix64F rotNeg90Z = new DenseMatrix64F(new double[][]{{0, 1, 0}, {-1, 0, 0}, {0, 0, 1}});
+                            DenseMatrix64F temp = CommonOps.identity(3);  // 3x3 zero-rotation matrix
+                            DenseMatrix64F new_targetToSensor_r = CommonOps.identity(3); // initialise to 3x3 zero-rotation matrix - overwritten below
+                            CommonOps.mult(rotNeg90Z, rotNeg90Y, temp);
+                            CommonOps.mult(targetToSensor.getR(), temp, new_targetToSensor_r);
+                            targetToSensor.setRotation(new_targetToSensor_r);
+
+                        // transform the translation
+                            Vector3D_F64 new_targetToSensor_t = new Vector3D_F64(0,0,0); // 3x1 zero-translation vector
+                            GeometryMath_F64.mult(temp, targetToSensor.getTranslation(), new_targetToSensor_t);
+
+
+                        Vector3D_F64 trans_TtoS = targetToSensor.getTranslation();
+                        Quaternion_F64 quat_TtoS = new Quaternion_F64();
+                        ConvertRotation3D_F64.matrixToQuaternion(targetToSensor.getR(), quat_TtoS);
+
+//                        detectedFeaturesClient.reportDetectedFeature(1000+tag_id, trans_TtoS.getX(), trans_TtoS.getY(), trans_TtoS.getZ(), quat_TtoS.x,quat_TtoS.y,quat_TtoS.z,quat_TtoS.w);
+
+//                        detectedFeaturesClient.reportDetectedFeature(2000+tag_id, trans_TtoS.getZ(), -trans_TtoS.getX(), -trans_TtoS.getY(), quat_TtoS.x,quat_TtoS.y,quat_TtoS.z,quat_TtoS.w);
+
+//                        detectedFeaturesClient.reportDetectedFeature(22000+tag_id, trans_TtoS.getZ(), -trans_TtoS.getX(), -trans_TtoS.getY(), quat_TtoS.z,-quat_TtoS.x,-quat_TtoS.y,quat_TtoS.w);
+
+
+                        sensorToTargetIn = null;
                         sensorToTarget = targetToSensor.invert(sensorToTargetIn);
                         Vector3D_F64 trans_StoT = sensorToTarget.getTranslation();
 //                        VisualizeFiducial.drawCube(targetToSensor, param, detector.getWidth(i), 3, g2);
 //                        VisualizeFiducial.drawLabelCenter(targetToSensor, param, "" + detector.getId(i), g2);
-                        org.ros.rosjava_geometry.Vector3 translation_to_tag_in_robot_convention
-                                = new org.ros.rosjava_geometry.Vector3(trans_StoT.getX(), trans_StoT.getY(), trans_StoT.getZ());
+//                        org.ros.rosjava_geometry.Vector3 translation_to_tag_in_robot_convention
+//                                = new org.ros.rosjava_geometry.Vector3(trans_StoT.getX(), trans_StoT.getY(), trans_StoT.getZ());
 //                        org.ros.rosjava_geometry.Vector3 translation_to_tag_in_robot_convention = new org.ros.rosjava_geometry.Vector3(trans.getZ(), trans.getX(), trans.getY());
 //                        org.ros.rosjava_geometry.Vector3 translation_to_tag_in_robot_convention = new org.ros.rosjava_geometry.Vector3(trans.getZ(), -trans.getX(), -trans.getY());
                         Quaternion_F64 quat_StoT = new Quaternion_F64();
@@ -636,6 +717,22 @@ public class MainActivity
 
                         System.out.println("3D Location: targetToSensor = ");  System.out.println(targetToSensor);  System.out.println(quatBoofCV_TtoS);
                         System.out.println("3D Location: sensorToTarget = ");  System.out.println(sensorToTarget);  System.out.println(quat_StoT);
+
+
+
+                        targetToSensor.setTranslation(new_targetToSensor_t);
+//                        detectedFeaturesClient.reportDetectedFeature(3000+tag_id, trans_TtoS.getZ(), -trans_TtoS.getX(), -trans_TtoS.getY(), quat_TtoS.z,-quat_TtoS.x,-quat_TtoS.y,-quat_TtoS.w);
+
+                        sensorToTargetIn = null;
+                        sensorToTarget = targetToSensor.invert(sensorToTargetIn);
+                        quat_StoT = new Quaternion_F64();
+                        ConvertRotation3D_F64.matrixToQuaternion(sensorToTarget.getR(), quat_StoT);
+                        trans_StoT = sensorToTarget.getTranslation();
+//                        detectedFeaturesClient.reportDetectedFeature(4000+tag_id, trans_StoT.getX(), trans_StoT.getY(), trans_StoT.getZ(), quat_StoT.x,quat_StoT.y,quat_StoT.z,quat_StoT.w);
+//                        detectedFeaturesClient.reportDetectedFeature(5000+tag_id, trans_StoT.getZ(), -trans_StoT.getX(), -trans_StoT.getY(), quat_StoT.x,quat_StoT.y,quat_StoT.z,quat_StoT.w);
+//                        detectedFeaturesClient.reportDetectedFeature(6000+tag_id, trans_StoT.getZ(), -trans_StoT.getX(), -trans_StoT.getY(), quat_StoT.z, -quat_StoT.x,-quat_StoT.y,-quat_StoT.w);
+
+
 
 //                        org.ros.rosjava_geometry.Quaternion quaternion_rotation_to_tag = new org.ros.rosjava_geometry.Quaternion(qz, -qx, -qy, qw);
 //                        org.ros.rosjava_geometry.Quaternion quaternion_rotation_to_tag = new org.ros.rosjava_geometry.Quaternion(quat_StoT.x,quat_StoT.y,quat_StoT.z,quat_StoT.w);
@@ -654,7 +751,9 @@ public class MainActivity
 //                        detectedFeaturesClient.reportDetectedFeature(tag_id, trans_StoT.getZ(), -trans_StoT.getX(), -trans_StoT.getY(), quat_StoT.z,-quat_StoT.x,-quat_StoT.y,quat_StoT.w);
 //                        detectedFeaturesClient.reportDetectedFeature(tag_id, transBoofCV_TtoS.getZ(), -transBoofCV_TtoS.getX(), -transBoofCV_TtoS.getY(), 0,0,0,1);
 //                        detectedFeaturesClient.reportDetectedFeature(tag_id, transBoofCV_TtoS.getX(), transBoofCV_TtoS.getY(), transBoofCV_TtoS.getZ(), 0,0,0,1);
+
                         detectedFeaturesClient.reportDetectedFeature(tag_id, trans_StoT.getX(), trans_StoT.getY(), trans_StoT.getZ(), quat_StoT.x,quat_StoT.y,quat_StoT.z,quat_StoT.w);
+
                         if (!poseKnown) {
 //                            localiseFromAFeatureClient.localiseFromAFeature(tag_id, trans_StoT.getX(), trans_StoT.getY(), trans_StoT.getZ(), quat_StoT.x,quat_StoT.y,quat_StoT.z,quat_StoT.w);
 //                            localiseFromAFeatureClient.localiseFromAFeature(tag_id, trans_StoT.getZ(), -trans_StoT.getX(), -trans_StoT.getY(), quat_StoT.z,-quat_StoT.x,-quat_StoT.y,quat_StoT.w);
