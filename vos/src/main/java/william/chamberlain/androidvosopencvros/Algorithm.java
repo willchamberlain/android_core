357a47aa402d4892008f4f933251389876175389
William Chamberlain <william.chamberlain@hdr.qut.edu.au>
2017-09-25 15:51:44 +1000

    Formatting: fix indentation.

--- vos/src/main/java/william/chamberlain/androidvosopencvros/Algorithm.java
+++ vos/src/main/java/william/chamberlain/androidvosopencvros/Algorithm.java
@@ -5,21 +5,20 @@ package william.chamberlain.androidvosopencvros;
  */
 
 public enum Algorithm {
-        APRIL_TAGS_KAESS_36_H_11("AprilTags_Kaess_36h11"),
-        BOOFCV_SQUARE_FIDUCIAL("boofcv square fiducial");
+    APRIL_TAGS_KAESS_36_H_11("AprilTags_Kaess_36h11"),
+    BOOFCV_SQUARE_FIDUCIAL("boofcv square fiducial");
 
-        private final String canonicalName;
+    private final String canonicalName;
 
-        private Algorithm(String canonicalName_) {
-            this.canonicalName = canonicalName_;
-        }
+    private Algorithm(String canonicalName_) {
+        this.canonicalName = canonicalName_;
+    }
 
-        public String canonicalName() {
-            return canonicalName;
-        }
+    public String canonicalName() {
+        return canonicalName;
+    }
 
-        public String toString() {
-            return canonicalName;
-        }
-
+    public String toString() {
+        return canonicalName;
+    }
 }
